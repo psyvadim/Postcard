4c48df15f03c4644838f7a36806fef312fa16e87
psyvadim <psyvadim@gmail.com>
2014-11-17 00:30:25 +0300

    Postcard
    Testing My First App For iOS6 via Swift Language

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextfield: UITextField!
+    @IBOutlet weak var enterMessage: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessage.text
+        messageLabel.textColor = UIColor.redColor()
+       
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextfield.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextfield.text = ""
+        enterNameTextfield.resignFirstResponder()
+        
+        enterMessage.text = ""
+        enterMessage.resignFirstResponder()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
 }
 
